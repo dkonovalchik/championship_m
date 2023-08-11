0eabd3557f0d7ab9bd12af04fe0514b2f20b3570
Dyukon <dyukon@gmail.com>
2023-08-11 12:01:01 +0600

    eslint/prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,19 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  plugins: ['prettier', '@typescript-eslint'],
   parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
+    ecmaVersion: 2021,
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+  overrides: [
+    {
+      files: ['*.js', '*.ts', '*.jsx', '*.tsx'],
+      rules: {
+        curly: 'error',
+      },
+    },
+  ],
 };
